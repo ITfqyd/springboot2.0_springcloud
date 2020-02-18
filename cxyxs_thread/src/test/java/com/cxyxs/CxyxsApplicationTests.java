f9b7b6af9e006c7913d8ddbd37613f02a294a85b
ITfqyd <myworked@163.com>
2020-02-18 16:48:26 +0800

    多线程代码

---
+++ cxyxs_thread/src/test/java/com/cxyxs/CxyxsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cxyxs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CxyxsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
